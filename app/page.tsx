f1d1339cfc63c622d7a70cac6823d0c0e6af2b6e
teleskop <teleskop@192.168.1.35>
2024-12-11 12:06:18 +0300

    CI CD pipeline

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchBarbers() {
       try {
-        const response = await fetch("http://localhost:8080/barbers")
+        const response = await fetch("http://34.142.51.130:8080/barbers")
         const data = await response.json()
         setBarbers(data)
       } catch (error) {
