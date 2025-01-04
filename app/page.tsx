d1cccf8e564a56033c31fdf8ec5eba60bcc86103
teleskop <teleskop@192.168.1.39>
2025-01-04 15:17:02 +0300

    docker file

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
     // Fetch Barbers
     async function fetchBarbers() {
       try {
-        const response = await fetch("http://34.142.51.130:8080/barbers")
+        const response = await fetch("http://localhost:8080/barbers")
         const data = await response.json()
         setBarbers(data)
       } catch (error) {
@@ -61,7 +61,7 @@ export default function Home() {
     // Fetch Districts
     async function fetchDistricts() {
       try {
-        const response = await fetch("http://34.142.51.130:8080/districts")
+        const response = await fetch("http://localhost:8080/districts")
         const data = await response.json()
         setDistricts(data)
       } catch (error) {
@@ -161,8 +161,7 @@ const CardItem = ({ barber }: { barber: Barber }) => {
     return availableTimes.map((time) => {
       const isUnavailable = (barber.appointments || []).some((appointment) => {
         const appointmentDate = appointment.date.split("T")[0]
-        const appointmentTime = appointment.slotTime
-        return appointmentDate === selectedDate && appointmentTime === time
+        return appointmentDate === selectedDate && appointment.slotTime === time
       })
       return { time, isUnavailable }
     })
